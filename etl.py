e69413fa89dd8612c07b7b671944c6587263bf39
Damoscky <seyiazure@gmail.com>
2025-02-04 11:22:47 -0700

    Added ETL script

---
+++ etl.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data ={'Name':{'Alice', 'Bob', 'Charlie'], 'Age' :[25,30,40]}
+df=pd.dataFrame(data)
+df.to_csv('Output.csv', index=False)
+
+print('ETL job completed. data saved to output.csv')
